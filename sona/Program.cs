e5844bf6abde0e9539c82ba98c4629932293da16
laolang2016 <xiaodaima2016@163.com>
2024-05-21 17:08:23 +0800

    ✨ feat(sona): 新建项目

---
+++ sona/Program.cs
@@ -0,0 +1,15 @@
+namespace sona;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        a = 2;
+        a = 3;
+        a = 4;
+        a = 5;
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+    }
+}
